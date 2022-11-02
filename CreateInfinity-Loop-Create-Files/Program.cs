f37e16ad0978f9eb1b36ca3b4c30e81d5244e1be
MohammedHawary <94152045+MohammedHawary@users.noreply.github.com>
2022-11-02 12:29:51 +0200

    Infinity file
    Infinity file

---
+++ CreateInfinity-Loop-Create-Files/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string fileName = "C:\\Users\\Public\\Desktop\\";
+string NewFileName = "";
+int x = 1;
+
+while (x != 50)
+{
+    NewFileName = fileName + "file_" + x.ToString() ;
+
+    using (FileStream fs = File.Create(NewFileName));
+    x++;
+}
+
+
+
